688bc2dcea969f6f843a27822a9f7ea85804be0c
bhagyaprasad92 <scs150831@gmail.com>
2026-06-20 21:39:36 +0530

    Fix dangling library doc comment for perfect pub.dev score

--- lib/offline_sos_system.dart
+++ lib/offline_sos_system.dart
@@ -1,4 +1,5 @@
 /// A pure Dart, offline-first Flutter package for on-device crash and anomaly detection.
+library offline_sos_system;
 
 export 'src/safepulse_engine.dart';
 export 'src/enums.dart';
